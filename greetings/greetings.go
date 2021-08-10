fdaea4695da3d918264501cb830dd5ae0d4aadbe
zhangyuguang <zhangyuguang@live.com>
2021-08-10 23:02:45 +0800

    import package hello

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -5,6 +5,6 @@ import "fmt"
 // 这里是注释部分
 func Hello(name string) string {
 	// := 是 var message string的简写
-	message := fmt.Sprintln("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
 }
