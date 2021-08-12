0a86cf1039fd0a6ca5db92402d58d36bdbcb0500
zhangyuguang <zhangyuguang@live.com>
2021-08-12 22:20:27 +0800

    add error

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,17 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // 这里是注释部分
-func Hello(name string) string {
+func Hello(name string) (string, error) {
 	// := 是 var message string的简写
+	//增加对错误的提示
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -1,12 +1,19 @@
 package main
 
 import (
-	"fmt"
-
 	"example.com/greetings"
+	"fmt"
+	"log"
 )
 
 func main() {
-	message := greetings.Hello("lzx")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	//message := greetings.Hello("lzx")
+	//test error
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }
